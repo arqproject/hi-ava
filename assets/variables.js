a510b2ca477979be3839d36369876a0c0394f1ee
arqproject <63571769+arqproject@users.noreply.github.com>
2020-05-23 16:24:06 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["❤", "gldys🤗❤", "Sayang🤗", "🤗", "Cintuu❤"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
