e7515489525a40723d338e34fae8074ebac79af5
arqproject <63571769+arqproject@users.noreply.github.com>
2020-05-23 16:22:25 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,8 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "ariqoh", quotes: "Minal Aidzin walfaizin Gldys 🤗❤" },
+    { author: "ariqoh", quotes: "Luv yuuu gldys" },
+    { author: "ariqoh", quotes: "Hey! I wuv chuuuu! UwU😂" },
+    { author: "ariqoh", quotes: "I love you." },
+    
 ];
